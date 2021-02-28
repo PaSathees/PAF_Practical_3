a0c35d73e2c628587f57e9430c698f49513e9375
PaSathees <sathpaskaran@gmail.com>
2021-02-28 13:28:42 +0530

    inserted readItems method to Item class

--- src/com/Item.java
+++ src/com/Item.java
@@ -50,4 +50,60 @@ public class Item {
 		
 		return output;		
 	}
+	
+	public String readItems() {
+		String output = "";
+		
+		try {
+			Connection con = this.connect();
+			if (con == null) {
+				return "Error while connecting to database for reading";
+			}
+			
+			//preparing HTML table as for output
+			output = "<table border='1'>"
+					+ "<tr>"
+					+ "<th>Item Code</th>"
+					+ "<th>Item Name</th>"
+					+ "<th>Item Price</th>"
+					+ "<th>Item Description</th>"
+					+ "<th>Update</th>"
+					+ "<th>Remove</th>"
+					+ "</tr>";
+			
+			String query = "select * from items";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			//iterate over each row in the result set
+			while (rs.next()) {
+				String itemID = Integer.toString(rs.getInt("itemID"));
+				String itemCode = rs.getString("itemCode");
+				String itemName = rs.getString("itemName");
+				String itemPrice = rs.getString("itemPrice");
+				String itemDesc = rs.getString("itemDesc");
+				
+				//add retrieved values to output table
+				output += "<tr><td>" + itemCode + "</td>";
+				output += "<td>" + itemName + "</td>";
+				output += "<td>" + itemPrice + "</td>";
+				output += "<td>" + itemDesc + "</td>";
+				
+				output += "<td><input name='btnUpdate' type='button' value='Update'></td>"
+						+ "<td><form method='post' action='item.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove'>"
+						+ "<input name='itemID' type='hidden' value='" + itemID + "'>"
+						+ "</form></td></tr>";
+			}
+			
+			con.close();
+			output += "</table>";
+			
+		} catch (Exception e) {
+			output = "Error while reading the items";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
 }
