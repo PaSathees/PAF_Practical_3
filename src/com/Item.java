90b799857e2683c8329095697b1520710bc32f35
PaSathees <sathpaskaran@gmail.com>
2021-02-28 11:16:14 +0530

    added connect method to Item class

--- src/com/Item.java
+++ src/com/Item.java
@@ -1,5 +1,18 @@
 package com;
+import java.sql.*;
 
 public class Item {
-
+	public Connection connect() {
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_lab3", "root", "");
+			System.out.println("Connection successfully established");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
 }
