c301e14ec2a8d2ee08f7d8001de334e734d3498d
PaSathees <sathpaskaran@gmail.com>
2021-02-28 11:22:04 +0530

    added insertItem method to Item class

--- src/com/Item.java
+++ src/com/Item.java
@@ -15,4 +15,39 @@ public class Item {
 		
 		return con;
 	}
+	
+	
+	public String insertItem(String code, String name, String price, String desc) {
+		String output = "";
+		
+		try {
+			Connection con = this.connect();
+			
+			if (con == null) {
+				return "Error connecting to database";
+			}
+			
+			//creating prepared statement
+			String query = "insert into items ('itemID', 'itemCode', 'itemName', 'itemPrice', 'itemDesc')"
+					+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			//binding values to prepared statement
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+			
+			preparedStmt.execute();
+			con.close();
+			
+			output = "inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;		
+	}
 }
